5549bca59e34705b9f90096fbef44723e1807671
Samuel Gomes <samuel.codepp@gmail.com>
2023-10-27 09:27:24 -0300

    Add package breeze e configurado registro, login e logout do usuário

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,19 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
 
-Route::prefix('tasks')
-    ->middleware('auth:sanctum')
-    ->group(function() {
-        Route::get('/', [\App\Http\Controllers\TasksController::class, 'index']);
-        Route::post('/', [\App\Http\Controllers\TasksController::class, 'store']);
-    });
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+require __DIR__ . '/auth.php';
